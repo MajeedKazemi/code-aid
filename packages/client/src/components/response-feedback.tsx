0f4417852124c34530187ff3f56a6d4053c749c4
MajeedKazemi <majeed@dgp.toronto.edu>
2023-01-13 12:43:11 -0500

    redesigned response feedback

--- packages/client/src/components/response-feedback.tsx
+++ packages/client/src/components/response-feedback.tsx
@@ -1,6 +1,7 @@
-import { useContext, useState } from "react";
+import { Fragment, useContext, useState } from "react";
 
 import { AuthContext } from "../context";
+import { getIconSVG } from "../utils/icons";
 
 interface IProps {
     responseId: string;
@@ -11,57 +12,134 @@ interface IProps {
 export const ResponseFeedback = (props: IProps) => {
     const { context } = useContext(AuthContext);
 
-    const [selectedOption, setSelectedOption] = useState<string | null>(null);
+    const [selectedNumber, setSelectedNumber] = useState(0);
+    const [hoveredNumber, setHoveredNumber] = useState(0);
     const [reason, setReason] = useState("");
     const [submitted, setSubmitted] = useState(false);
 
     if (submitted) return null;
 
     return (
-        <div>
-            <span>how useful was this response?</span>
-            <span
-                className={
-                    "feedback-option-yes " +
-                    (selectedOption === "yes"
-                        ? "feedback-option-selected-yes"
-                        : "")
-                }
-                onClick={() => {
-                    setSelectedOption("yes");
-                }}
-            >
-                yes
-            </span>
-            <span
-                className={
-                    "feedback-option-no " +
-                    (selectedOption === "no"
-                        ? "feedback-option-selected-no"
-                        : "")
-                }
-                onClick={() => {
-                    setSelectedOption("no");
-                }}
-            >
-                no
-            </span>
-
-            {selectedOption !== null ? (
-                <textarea
-                    value={reason}
-                    onChange={(e) => {
-                        setReason(e.target.value);
+        <div className="response-feedback-container">
+            <div className="response-feedback-header">
+                how useful was this response?
+            </div>
+            <div className="response-feedback-content">
+                <div
+                    className="star-icons-container"
+                    onMouseOut={() => {
+                        setHoveredNumber(0);
                     }}
-                ></textarea>
-            ) : null}
-            <button
-                onClick={() => {
-                    setSubmitted(true);
-                }}
-            >
-                submit
-            </button>
+                >
+                    <span
+                        className="single-star-icon-container"
+                        onMouseOver={() => {
+                            setHoveredNumber(1);
+                        }}
+                        onClick={() => {
+                            setSelectedNumber(1);
+                        }}
+                    >
+                        {getIconSVG(
+                            "star" +
+                                (hoveredNumber >= 1 || selectedNumber >= 1
+                                    ? "-filled"
+                                    : "-stroke"),
+                            selectedNumber >= 1 ? "response-star-selected" : ""
+                        )}
+                    </span>
+                    <span
+                        className="single-star-icon-container"
+                        onMouseOver={() => {
+                            setHoveredNumber(2);
+                        }}
+                        onClick={() => {
+                            setSelectedNumber(2);
+                        }}
+                    >
+                        {getIconSVG(
+                            "star" +
+                                (hoveredNumber >= 2 || selectedNumber >= 2
+                                    ? "-filled"
+                                    : "-stroke"),
+                            selectedNumber >= 2 ? "response-star-selected" : ""
+                        )}
+                    </span>
+                    <span
+                        className="single-star-icon-container"
+                        onMouseOver={() => {
+                            setHoveredNumber(3);
+                        }}
+                        onClick={() => {
+                            setSelectedNumber(3);
+                        }}
+                    >
+                        {getIconSVG(
+                            "star" +
+                                (hoveredNumber >= 3 || selectedNumber >= 3
+                                    ? "-filled"
+                                    : "-stroke"),
+                            selectedNumber >= 3 ? "response-star-selected" : ""
+                        )}
+                    </span>
+                    <span
+                        className="single-star-icon-container"
+                        onMouseOver={() => {
+                            setHoveredNumber(4);
+                        }}
+                        onClick={() => {
+                            setSelectedNumber(4);
+                        }}
+                    >
+                        {getIconSVG(
+                            "star" +
+                                (hoveredNumber >= 4 || selectedNumber >= 4
+                                    ? "-filled"
+                                    : "-stroke"),
+                            selectedNumber >= 4 ? "response-star-selected" : ""
+                        )}
+                    </span>
+                    <span
+                        className="single-star-icon-container"
+                        onMouseOver={() => {
+                            setHoveredNumber(5);
+                        }}
+                        onClick={() => {
+                            setSelectedNumber(5);
+                        }}
+                    >
+                        {getIconSVG(
+                            "star" +
+                                (hoveredNumber >= 5 || selectedNumber >= 5
+                                    ? "-filled"
+                                    : "-stroke"),
+                            selectedNumber >= 5 ? "response-star-selected" : ""
+                        )}
+                    </span>
+                </div>
+                {selectedNumber !== 0 ? (
+                    <textarea
+                        placeholder="please explain why"
+                        className="feedback-reason-textarea"
+                        value={reason}
+                        onChange={(e) => {
+                            setReason(e.target.value);
+                        }}
+                    ></textarea>
+                ) : null}
+
+                <span
+                    className={
+                        "submit-feedback-button " +
+                        (selectedNumber == 0 ? "feedback-button-disabled" : "")
+                    }
+                    onClick={() => {
+                        setSubmitted(true);
+                    }}
+                >
+                    submit
+                </span>
+            </div>
         </div>
     );
 };
